0c07807f295e1f35fb4e73a4d65d3e9944865cf1
Annes Silde <47981031+annes99@users.noreply.github.com>
2019-05-05 17:35:13 +0700

    add portfolio, update readme

--- README.md
+++ README.md
@@ -11,6 +11,11 @@
 * Error handling
 * Server to serve the project
 
+### `npm start`
+
+Runs the app in the development mode.<br>
+Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
+
 ## Technologies
 
 * JavaScript
@@ -22,22 +27,27 @@
 
 ## Demo
 
-## `npm start`
-
-Runs the app in the development mode.<br>
-Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
+Active demo: [Static NodeJS and Express Site](https://p6-test.herokuapp.com/)
 
 ## Screenshot
 
+![Example screenshot](public/img/portfolio/portfolio1200_1.png)
+
 ## Status
 
-Project is: _IN PROGRESS_
+Project is: _completed_
 
 ## Inspiration
 
 Treehouse Techdegree: FSJS project 6 - Static NodeJS and Express Site
 
 ## CSS style changes
+
+* body background to rgb(245, 247, 250) & font-family: 'Montserrat', sans-serif;
+* a:hover color: #111 to rgb(12, 93, 242);
+* button background-color: #333 to rgb(12, 93, 242) + border-radius: 8px;
+* a color: #333 to rgb(12, 93, 242)
+* added favicon icon
 
 ## Contact
 
--- app.js
+++ app.js
@@ -1,33 +1,34 @@
+// requireing Express and JSON file
 const express = require('express');
 const { projects } = require('./data.json');
+
+// setting port to 3000 or PORT to tell web server what port to listen (for Heroku)
 const port = process.env.PORT || 3000;
 const app = express();
 
 /**** Set up middleware ****/
 
-// use a static route and the express.static method to serve the static files located in the public folder
+// using express.static method to serve the static files located in the public folder
 app.use('/static', express.static('public'));
 
-// set your “view engine” to “pug”
+// setting “view engine” to “pug”
 app.set('view engine', 'pug');
 
 /**** Set routes ****/
 
-// An "index" route (/) to render the "Home" page with the locals set to data.projects
+// '/' route render the index page with the locals set to properties
 app.get('/', (req, res) => {
     res.render('index', { projects }  );
 });
 
-// An "about" route (/about) to render the "About" page
+// '/about' route to render the "About" page
 app.get('/about', (req, res) => {
     res.render('about');
 });
 
-
-// Dynamic "project" routes (/project or /projects) based on the id of the project that 
-// render a customized version of the Pug project template to show off each project. 
-// Which means adding data, or "locals", as an object that contains data to be passed to the Pug template.
-
+// '/project' route to dynamically change based on projects id
+// if id is in the range of actual projects then render 'project'
+// else => next() render 'error'
 app.get('/project/:id', (req, res, next) => {
     const { id } = req.params;
     const data = projects[id];
@@ -41,6 +42,7 @@ app.get('/project/:id', (req, res, next) => {
 
 /**** error handling ****/
 
+// for non-existent route render 'error'
 app.use((req, res, next) => {
     const err = new Error('Page not found');
     err.status = 404;
